aae060d3ecfe253b8c4bb1cb114aa92e14ba24b9
jilong <jilong@192.168.1.106>
2013-07-28 15:16:52 +0800

    环境

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-TOM_HOME=/Users/Hong/work/java/apache-tomcat-7.0.41
+TOM_HOME=/Users/jilong/Downloads/dev/apache-tomcat-6.0.16
 echo "rm -rf $TOM_HOME/webapps/transmit"
 rm -rf $TOM_HOME/webapps/transmit
 echo "cp -r WebRoot $TOM_HOME/webapps/transmit"
